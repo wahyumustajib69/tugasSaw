44c96a3e1ce95dca0f07e585f075abb2f52e3c41
wahyumustajib69 <adjieb.wahyu@gmail.com>
2017-12-31 17:46:18 +0800

    Hapus yang tidak kepakai lagi

--- SAW.cpp
+++ SAW.cpp
@@ -5,14 +5,6 @@ using namespace std;
 int main(){
 	// DEKLARASI VARIABEL
 	double a[3][5]={{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
-	
-	double Ra1c1,Ra1c2,Ra1c3,Ra1c4,Ra1c5;
-	double Ra2c1,Ra2c2,Ra2c3,Ra2c4,Ra2c5;
-	double Ra3c1,Ra3c2,Ra3c3,Ra3c4,Ra3c5;
-	
-	double RWa1c1,RWa1c2,RWa1c3,RWa1c4,RWa1c5;
-	double RWa2c1,RWa2c2,RWa2c3,RWa2c4,RWa2c5;
-	double RWa3c1,RWa3c2,RWa3c3,RWa3c4,RWa3c5;
 		
 	double w[5]={5,3,4,4,2};
 	double mc1,mc2,mc3,mc4,mc5;
@@ -22,9 +14,7 @@ int main(){
 	cout.setf(ios::fixed);cout.setf(ios::showpoint);cout.precision(4);
 	
 	// PENJUMLAHAN HASIL R * W	
-	ha1=RWa1c1+RWa1c2+RWa1c3+RWa1c4+RWa1c5;
-	ha2=RWa2c1+RWa2c2+RWa2c3+RWa2c4+RWa2c5;
-	ha3=RWa3c1+RWa3c2+RWa3c3+RWa3c4+RWa3c5;
+
 		
 	// MENAMPILKAN NILAI
 	cout << "MATRIKS X :"<<endl;
@@ -64,9 +54,7 @@ int main(){
 	cout << endl;
 	cout << endl;
 	cout << "MATRIKS R * W :"<<endl;
-	cout << RWa1c1 <<" | "<<RWa1c2<<" | "<<RWa1c3<<" | "<<RWa1c4<<" | "<<RWa1c5<<endl;
-	cout << RWa2c1 <<" | "<<RWa2c2<<" | "<<RWa2c3<<" | "<<RWa2c4<<" | "<<RWa2c5<<endl;
-	cout << RWa3c1 <<" | "<<RWa3c2<<" | "<<RWa3c3<<" | "<<RWa3c4<<" | "<<RWa3c5<<endl;
+	
 	cout << endl;
 	cout << "HASIL AKHIR  :"<<endl;
 	cout << "Nilai A1 : "<<ha1<<endl;
